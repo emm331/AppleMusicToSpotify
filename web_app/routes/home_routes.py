0eeb0783f079084afd8fa11bf6814cc839b1014c
andrewp113 <asp113@georgetown.edu>
2022-12-05 23:19:18 -0500

    Updates to web app - but incomplete

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -11,25 +11,26 @@ def index():
     #return "Welcome Home"
     return render_template("home.html")
 
-@home_routes.route("/about")
-def about():
+@home_routes.route("/convert")
+def convert():
     print("ABOUT...")
     #return "About Me"
-    return render_template("about.html")
+    return render_template("convert.html")
 
-@home_routes.route("/hello")
-def hello_world():
-    print("HELLO...")
 
-    # if the request contains url params, for example a request to "/hello?name=Harper"
-    # the request object's args property will hold the values in a dictionary-like structure
-    url_params = dict(request.args)
-    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
+# @home_routes.route("/hello")
+# def hello_world():
+#     print("HELLO...")
 
-    # get a specific key called "name" if available, otherwise use some specified default value
-    # see also: https://www.w3schools.com/python/ref_dictionary_get.asp
-    name = url_params.get("name") or "World"
+#     # if the request contains url params, for example a request to "/hello?name=Harper"
+#     # the request object's args property will hold the values in a dictionary-like structure
+#     url_params = dict(request.args)
+#     print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
 
-    message = f"Hello, {name}!"
-    #return message
-    return render_template("hello.html", message=message)
+#     # get a specific key called "name" if available, otherwise use some specified default value
+#     # see also: https://www.w3schools.com/python/ref_dictionary_get.asp
+#     name = url_params.get("name") or "World"
+
+#     message = f"Hello, {name}!"
+#     #return message
+#     return render_template("hello.html", message=message)
