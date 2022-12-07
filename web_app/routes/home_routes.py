e7591669ef196a612cd4ef5ad0487e388e37b79c
emm331 <emm331@georgetown.edu>
2022-12-06 21:12:13 -0500

    Deleting unnecessary files

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,11 +8,9 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/")
 @home_routes.route("/home")
 def index():
+    print("Home")
     return render_template("home.html")
 
 @home_routes.route("/convert")
 def convert():
     return render_template("convert.html")
-
-
-
